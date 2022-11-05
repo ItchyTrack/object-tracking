685f7ca70efcd41a3fd159cf698218490d75a169
ksbidlack <ksbidlack@gmail.com>
2022-11-04 19:15:05 -0700

    Cleaned up some code

--- main.py
+++ main.py
@@ -2,33 +2,42 @@ import cv2
 import os
 import sys
 from tkinter import filedialog
+import shutil
 
-video_path = filedialog.askopenfilename()
-capture = cv2.VideoCapture(video_path)
-frame_number = 0
-total_video_frames = int(capture.get(cv2.CAP_PROP_FRAME_COUNT)) # this doesn't work
+def create_frames():
+    print("Select a video")
+    video_path = filedialog.askopenfilename()
+    capture = cv2.VideoCapture(video_path)
+    total_video_frames = int(capture.get(cv2.CAP_PROP_FRAME_COUNT))
 
-try:
-    os.mkdir('/tmp/frames')
-except FileExistsError:
-    delete_tmp_frames = input("/tmp/frames exists. Overwrite it? (y/n)")
-    if delete_tmp_frames == "y":
-        os.rmdir('/tmp/frames')
-    elif delete_tmp_frames == "n":
-        print("Using frames specified in /tmp/frames.")
+    print(f"Generating frames from video: {video_path}")
+    frame_number = 0
+    
+    while True:
+        # process frames
+        success, frame = capture.read()
 
-print(f"Generating frames from video: {video_path}/{total_video_frames}")
+        if frame_number % 60 == 0:
+            print(f"Splitting frames... {frame_number}/{total_video_frames}")
 
-while True:
-    # process frames
-    success, frame = capture.read()
+        if success:
+            cv2.imwrite(f"/tmp/frames/frame{frame_number}.jpg", frame)
+        else:
+            break # finish loop at the end of the video
 
-    if frame_number % 60 == 0:
-        print(f"Splitting frames... {frame_number}/")
+        frame_number += 1
 
-    if success:
-        cv2.imwrite(f"/tmp/frames/frame_{frame_number}.jpg", frame)
-    else:
-        break # break at the end of the video
 
-    frame_number += 1
+if __name__ == "__main__":
+    # temporary folder for split frames
+    try:
+        os.mkdir('/tmp/frames')
+        create_frames()
+    except FileExistsError: # use pre-processed frames
+        delete_tmp_frames = input("/tmp/frames exists. Overwrite it? (y/n) ")
+        if delete_tmp_frames == "y":
+            shutil.rmtree('/tmp/frames')
+            os.mkdir('/tmp/frames')
+            create_frames()
+        elif delete_tmp_frames == "n":
+            print("Using frames specified in /tmp/frames.")
