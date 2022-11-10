32cab731b95859f4871fca7693bec1bd3faa078d
Ben Herman <bherman24@dtechhs.org>
2022-11-10 13:24:23 -0800

    now is way fast

--- main.py
+++ main.py
@@ -13,7 +13,7 @@ import scanner
 
 def create_frames():
     print("Select a video")
-    video_path = "/Users/ben/Documents/GitHub/object-tracking/red.mp4"
+    video_path = "./red.mp4"
     capture = cv2.VideoCapture(video_path)
     total_video_frames = int(capture.get(cv2.CAP_PROP_FRAME_COUNT))
 
@@ -28,7 +28,7 @@ def create_frames():
             print(f"Splitting frames... {frame_number}/{total_video_frames}")
 
         if success:
-            cv2.imwrite(f"/tmp/frames/frame{frame_number}.jpg", frame)
+            cv2.imwrite(f"./tmp/frames/frame{frame_number}.jpg", frame)
         else:
             break # finish loop at the end of the video
 
@@ -39,16 +39,16 @@ def create_frames():
 if __name__ == "__main__":
     # temporary folder for split frames
     try:
-        os.mkdir('/tmp/frames')
+        os.mkdir('./tmp/frames')
         create_frames()
     except FileExistsError: # use pre-processed frames
-        delete_tmp_frames = input("/tmp/frames exists. Overwrite it? (y/n) ")
+        delete_tmp_frames = input("./tmp/frames, Overwrite it? (y/n) ")
         if delete_tmp_frames == "y":
-            shutil.rmtree('/tmp/frames')
-            os.mkdir('/tmp/frames')
+            shutil.rmtree('./tmp/frames')
+            os.mkdir('./tmp/frames')
             create_frames()
         elif delete_tmp_frames == "n":
-            print("Using frames specified in /tmp/frames.")
+            print("Using frames specified in ./tmp/frames.")
 
 def addLine(img, x1, y1, x2, y2):
     point = Image.new(mode='RGB', size=(1, 1), color=(0, 255, 0))
@@ -61,11 +61,11 @@ def addLine(img, x1, y1, x2, y2):
     return img
 
 
-images = os.listdir('/tmp/frames')
+images = os.listdir('./tmp/frames')
 
 numberOfFrames = 0
-for i in range(math.floor(len(images)/2)):
-    pimage = Image.open(f'/tmp/frames/frame{i*2}.jpg')
+for i in range(math.floor(len(images))):
+    pimage = Image.open(f'./tmp/frames/frame{i}.jpg')
     foundPoints = scanner.scanImage(pimage)
     print(i)
     numberOfFrames += 1
@@ -73,6 +73,6 @@ for i in range(math.floor(len(images)/2)):
     pimage = addLine(pimage, foundPoints[0][0], foundPoints[1][1], foundPoints[1][0], foundPoints[1][1])
     pimage = addLine(pimage, foundPoints[0][0], foundPoints[0][1], foundPoints[0][0], foundPoints[1][1])
     pimage = addLine(pimage, foundPoints[1][0], foundPoints[0][1], foundPoints[1][0], foundPoints[1][1])
-    pimage.save(f'/tmp/newframes/frame{i}.jpg')
+    pimage.save(f'./tmp/newframes/frame{60}.jpg')
     
-os.system(f"cd /tmp/newframes && ffmpeg -r {numberOfFrames} -i frame%d.jpg /Users/ben/Documents/GitHub/object-tracking/predictedVideos")
+os.system(f"cd ./tmp/newframes && ffmpeg -r {numberOfFrames} -i frame%d.jpg ./predictedVideos")
